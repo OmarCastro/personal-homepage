2ff6e4592ed8e29c5daca514e74f51afc7e75ec5
Omar Castro <omar.castro.360@gmail.com>
2014-08-21 00:25:00 +0100

    split .less to different files, cache pages when js is enabled so the user can change it witout reloading, navigation works with css alone on smartphones and tablets

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ var filesize = require('gulp-filesize');
 var less = require('gulp-less');  
 var changed = require('gulp-changed');  
 var watch = require('gulp-watch');
+var refresh = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
 
 gulp.task('clean', function () {  
   return gulp.src('build', {read: false})
@@ -28,7 +31,7 @@ gulp.task('vendor', function() {
 });
 
 gulp.task('css', function () {  
-  return gulp.src('less/**/*.less')
+  return gulp.src('less/main.less')
     .pipe(changed('build/css'))
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
@@ -39,15 +42,29 @@ gulp.task('css', function () {
 
 gulp.task('css:watch', function () {  
   watch({
-    glob: 'css/**/*.less',
+    glob: 'css/*.less',
     emit: 'one',
     emitOnGlob: false
   }, function(files) {
-    return files
-      .pipe(less({
-        paths: [ path.join(__dirname, 'less', 'includes') ]
-      }))
+    gulp.src('css/main.less')
+      .pipe(less({}))
       .pipe(gulp.dest('css'))
       .on('error', gutil.log);
   });
 }); 
+
+gulp.task('jw', function(){
+    var spawn = require('child_process').spawn,
+        j     = spawn('jekyll', ['-w', 'build']);
+
+    j.stdout.on('data', function (data) {
+        console.log('stdout: ' + data); // works fine
+    });
+
+    watch({glob: '/glob/path/to/jekyll/output'}, function(files) {
+        // update files in batch mode
+        return files.pipe(refresh(server));
+    });
+});
+
+gulp.task('default', ['jw', 'css:watch']);
--- js/main.js
+++ js/main.js
@@ -1,88 +1,59 @@
 var $ = document.querySelector.bind(document),
-    $all = document.querySelectorAll.bind(document)
-    homeButton = $(".home-button"),
-    nav = $(".navigation"),
-    pages = {
-  main: $('[data-page="main"]'),
-  about: $('[data-page="about"]'),
-  skills: $('[data-page="skills"]'),
-  works: $('[data-page="works"]'),
-  contact: $('[data-page="contact"]')
-};
-
-//$(".menu-button").addEventListener("click", function(ev){
-//  ev.stopPropagation();
-//  nav.classList.toggle("active");
-//});
-//
-//document.body.addEventListener("click", function(ev){
-//  ev.stopPropagation();
-//  nav.classList.remove("active");
-//});
-
-var current = $(".page.current");
-var previous = current;
-
-var pagelist=[];
-for(var name in pages){
-  pages[name].setAttribute("data-index",pagelist.length);
-  pagelist.push(pages[name]);
+    $all = document.querySelectorAll.bind(document),
+    pages = ["","about","skills","works","contact"],
+    pageInfo = [];
+Element.prototype.on = Element.prototype.addEventListener;
+NodeList.prototype.on = function(event, fn, val){
+  for (var i = 0, len = this.length; i < len; i++) {
+    this[i].addEventListener(event, fn, val);
+  }
 }
 
-function setpage(page) {
-  document.body.setAttribute("data-onpage",page.getAttribute("data-page"));
-  if(current == page){
-    return; 
+
+if(isMobile.phone){
+  document.body.classList.add("phone", "mobile", "no-desktop", "no-tablet");
+} else if(isMobile.tablet){
+  document.body.classList.add("no-phone", "mobile", "no-desktop", "tablet");
+} else {
+  document.body.classList.add("no-phone", "no-mobile", "desktop", "no-tablet");
+}
+
+pages.forEach(function(val){
+  var request = new XMLHttpRequest();
+  request.open("GET", "./"+val, false);
+  request.send(null);
+  var doc = document.implementation.createHTMLDocument("example");
+  doc.documentElement.innerHTML = request.responseText;
+  pageInfo.push({
+    html: request.responseText,
+    pageHTML: doc.documentElement.querySelector(".page.current").outerHTML,
+    title: doc.documentElement.querySelector("title").innerHTML,
+    link: val
+  });
+});
+
+function gotoPage(pageinfo){
+  document.title = pageinfo.title;
+  $(".page.current").outerHTML = pageinfo.pageHTML
+  $(".pagelink.active").classList.remove("active");
+  $(".topbar-button.pagelink[data-to=\""+pageinfo.link+"\"]").classList.add("active");
+}
+
+$all(".pagelink").on("click", function(ev){
+  ev.stopPropagation();
+  ev.preventDefault();
+  ev = ev || window.event;
+  var target = ev.target || ev.srcElement
+  if (target.nodeType == 3){ // defeat Safari bug
+    target = target.parentNode;
   }
-  var classList = current.classList;
-
-  previous.classList.remove("previous");
-  classList.add("previous");
-  classList.add("out-of-current");
-  classList.remove("current");
-  setTimeout(function(){
-    classList.remove("out-of-current");
-  },700);
-  page.classList.add("current");
-  previous = current;
-  current = page;
-  page.scrollTop = 0;
-};
-
-function gotoNextPage(){
-  var index = parseInt(current.getAttribute("data-index")) || 0;
-  if(index < pagelist.length - 1){
-    location.hash = pagelist[index+1].getAttribute("data-page");
+  if(target.href == location.href){
+    return;
   }
-};
+  var href = target.getAttribute("data-to");
+  var info = pageInfo[pages.indexOf(href)];
+  gotoPage(info)
+  window.history.pushState({"info":info}, info.title , target.href);
+})
 
-function gotoPreviousPage(){
-  var index = parseInt(current.getAttribute("data-index")) || 0;
-  if(index > 0){
-    location.hash = pagelist[index-1].getAttribute("data-page");
-  }
-};
-
-function setPageOnHashValue() {
-  "use strict";
-  var elem, hashval = location.hash.substr(1);
-  if (pages[hashval]) {
-    setpage(pages[hashval]); 
-  } else {
-    setpage(pages.main); 
-  }
-};
-
-document.onkeydown = function checkKey(e) {
-    e = e || window.event;
-    if (e.keyCode == 37 /*left arrow*/) { gotoPreviousPage(); }
-    else if (e.keyCode == 39 /*right arrow*/) { gotoNextPage(); }
-};
-
-window.onhashchange = setPageOnHashValue
-setPageOnHashValue();
-
-var mc = new Hammer.Manager(document.body, {});
-mc.add(new Hammer.Swipe({direction: Hammer.DIRECTION_HORIZONTAL, velocity:0.3}));
-mc.on('swipeleft', gotoNextPage);
-mc.on('swiperight', gotoPreviousPage);
+window.onpopstate = function(event){ gotoPage(event.state.info); }
