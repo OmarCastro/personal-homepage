0de73df7d7c10b84eb638f004d30cf57b8270fa0
Omar Castro <omar.castro.360@gmail.com>
2014-08-22 01:11:06 +0100

    add CV, added info on "about me" page

--- js/main.js
+++ js/main.js
@@ -13,15 +13,6 @@ this.tablet=this.apple.tablet||this.android.tablet||this.windows.tablet},o=new n
 o.Class=n,"undefined"!=typeof module&&module.exports?module.exports=o:"function"==typeof define
 &&define.amd?define(o):a.isMobile=o}(this);
 
-//GoogleAnalytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-51662040-2', 'auto');
-ga('send', 'pageview');
-
 var $ = document.querySelector.bind(document),
     $all = document.querySelectorAll.bind(document),
     nav = $(".navigation"),
