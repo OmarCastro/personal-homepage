df4a3bc8c4ed0281d3294585d0fe3f31c48dbc07
Omar Castro <omar.castro.360@gmail.com>
2023-02-21 16:06:25 +0000

    simplify main.js

--- js/main.js
+++ js/main.js
@@ -1,88 +1,65 @@
-var $ = document.querySelector.bind(document),
-    $all = document.querySelectorAll.bind(document)
-    homeButton = $(".home-button"),
-    nav = $(".navigation"),
-    pages = {
-  main: $('[data-page="main"]'),
-  about: $('[data-page="about"]'),
-  skills: $('[data-page="skills"]'),
-  works: $('[data-page="works"]'),
-  contact: $('[data-page="contact"]')
+const $ = document.querySelector.bind(document)
+const $all = document.querySelectorAll.bind(document)
+
+const pages = {
+  main: $('.page--main'),
+  about: $('.page--about'),
+  skills: $('.page--skills'),
+  works: $('.page--works'),
+  contact: $('.page--contact'),
+  get current() { return $(".page:target") || $('.page--main') }
 };
 
-//$(".menu-button").addEventListener("click", function(ev){
-//  ev.stopPropagation();
-//  nav.classList.toggle("active");
-//});
-//
-//document.body.addEventListener("click", function(ev){
-//  ev.stopPropagation();
-//  nav.classList.remove("active");
-//});
-
-var current = $(".page.current");
-var previous = current;
-
-var pagelist=[];
-for(var name in pages){
-  pages[name].setAttribute("data-index",pagelist.length);
-  pagelist.push(pages[name]);
+function navigateToMainPage(){
+  location.hash = ""
+  history.replaceState('', document.title, window.location.pathname)
 }
 
-function setpage(page) {
-  document.body.setAttribute("data-onpage",page.getAttribute("data-page"));
-  if(current == page){
-    return; 
-  }
-  var classList = current.classList;
-
-  previous.classList.remove("previous");
-  classList.add("previous");
-  classList.add("out-of-current");
-  classList.remove("current");
-  setTimeout(function(){
-    classList.remove("out-of-current");
-  },700);
-  page.classList.add("current");
-  previous = current;
-  current = page;
-  page.scrollTop = 0;
-};
+const pagelist = Object.freeze(Object.keys(pages).filter(name => name !== "current").map(name => pages[name]))
 
 function gotoNextPage(){
-  var index = parseInt(current.getAttribute("data-index")) || 0;
-  if(index < pagelist.length - 1){
-    location.hash = pagelist[index+1].getAttribute("data-page");
-  }
+  const index = pagelist.indexOf(pages.current);
+  if(index >= pagelist.length - 1){ return }
+  const newId = pagelist[index+1].getAttribute("id")
+  newId == null ? navigateToMainPage() : location.hash = newId;
 };
 
 function gotoPreviousPage(){
-  var index = parseInt(current.getAttribute("data-index")) || 0;
-  if(index > 0){
-    location.hash = pagelist[index-1].getAttribute("data-page");
+  const index = pagelist.indexOf(pages.current);
+  if(index <= 0){ return }
+  const newId = pagelist[index-1].getAttribute("id")
+  newId == null ? navigateToMainPage() : location.hash = newId;
+};
+
+document.addEventListener('keydown', () => {
+  switch(e.key){
+    case "ArrowLeft": gotoPreviousPage(); break
+    case "ArrowRight": gotoNextPage(); break
   }
-};
+});
 
-function setPageOnHashValue() {
-  "use strict";
-  var elem, hashval = location.hash.substr(1);
-  if (pages[hashval]) {
-    setpage(pages[hashval]); 
-  } else {
-    setpage(pages.main); 
+window.addEventListener('hashchange', () => {
+  current = $(".page:target") || $('.page--main');
+});
+    
+function checkDirection(touchstartX, touchendX) {
+  if (touchendX < touchstartX) { gotoNextPage() }
+  if (touchendX > touchstartX) { gotoPreviousPage() }
+}
+
+document.addEventListener('touchstart', e => {
+  const touchstartX = e.changedTouches[0].screenX
+  let touchState = "startSwipe"
+  document.addEventListener('scroll', () => {touchState = "scrolling"}, {once: true, capture: true})
+  document.addEventListener('touchend', e => {
+    const touchendX = e.changedTouches[0].screenX
+    if(touchState === "startSwipe"){ checkDirection(touchstartX, touchendX) }
+  }, {once: true})
+})
+
+document.addEventListener('click', e => {
+  if(e.target.closest('.home-button')){
+    e.preventDefault()
+    history.pushState('', document.title, window.location.pathname)
   }
-};
-
-document.onkeydown = function checkKey(e) {
-    e = e || window.event;
-    if (e.keyCode == 37 /*left arrow*/) { gotoPreviousPage(); }
-    else if (e.keyCode == 39 /*right arrow*/) { gotoNextPage(); }
-};
-
-window.onhashchange = setPageOnHashValue
-setPageOnHashValue();
-
-var mc = new Hammer.Manager(document.body, {});
-mc.add(new Hammer.Swipe({direction: Hammer.DIRECTION_HORIZONTAL, velocity:0.3}));
-mc.on('swipeleft', gotoNextPage);
-mc.on('swiperight', gotoPreviousPage);
+})
