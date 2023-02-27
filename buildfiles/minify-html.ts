b387946393c2e76d33419a49d7b3c0e41a77bca2
Omar Castro <omar.castro.360@gmail.com>
2023-02-27 01:59:55 +0000

    add minify-html.ts

---
+++ buildfiles/minify-html.ts
@@ -0,0 +1,17 @@
+import {DOMParser, Element} from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { minifyHTML } from "https://deno.land/x/minifier/mod.ts";
+
+
+const projectDir = new URL("..", import.meta.url).pathname
+const content = await Deno.readFile(`${projectDir}/index.html`)
+const decoder = new TextDecoder("utf-8");
+
+
+const doc = new DOMParser().parseFromString(decoder.decode(content),"text/html")!;
+  
+console.log(minifyHTML(doc.documentElement!.outerHTML, {
+  minifyCSS: true,
+  minifyJS: true
+}));
+
+  
