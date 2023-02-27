ff91bc4707126275c08cc278a899811a1f713017
Omar Castro <omar.castro.360@gmail.com>
2023-02-27 22:11:38 +0000

    add build step

--- buildfiles/scripts/minify-html.ts
+++ buildfiles/scripts/minify-html.ts
@@ -2,7 +2,7 @@ import {DOMParser, Element} from "https://deno.land/x/deno_dom/deno-dom-wasm.ts"
 import { minifyHTML } from "https://deno.land/x/minifier/mod.ts";
 
 
-const projectDir = new URL("..", import.meta.url).pathname
+const projectDir = new URL("../..", import.meta.url).pathname
 const content = await Deno.readFile(`${projectDir}/index.html`)
 const decoder = new TextDecoder("utf-8");
 
